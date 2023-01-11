5903c08ce54807aadce83d14dad10fa436c6fbfb
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-11 23:22:21 +0530

    Added:Logic for connection between the java and database

--- src/com/java/database/Jdbc.java
+++ src/com/java/database/Jdbc.java
@@ -1,7 +1,39 @@
 package com.java.database;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class Jdbc {
 	public static void main(String[] args) {
-		System.out.println("Welcomr to employee_Payroll Jdbc program");
-}
+		System.out.println("Welcomr to Payroll_Service Jdbc program");
+
+		String url = "jdbc:mysql://localhost:3306/payroll_services?useSSL=false";
+		String username = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver Loaded!!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("cannot find Driver in classpath... " + e);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connection url: " + url);
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection Successfull!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
+	}
 }
