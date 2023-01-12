b92c4ba78404e74abb38b6574a0c2599c6ad9075
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-12 12:44:01 +0530

    Added:Logic for finding the sum, average, min,max,count for employee_payrool table

--- src/com/java/database/Jdbc.java
+++ src/com/java/database/Jdbc.java
@@ -18,6 +18,11 @@ public class Jdbc {
 		updateData(connection);
 		System.out.println(reteriveDataByName(connection));
 		particularDateRange(connection);
+		sumByGroup(connection);
+		avgSalary(connection);
+		minmumSalary(connection);
+		maxSalary(connection);
+		count(connection);
 	}
 
 	public static Connection connected() {
@@ -51,45 +56,116 @@ public class Jdbc {
 			System.out.println(resultSet.getString("name"));
 		}
 	}
+
 	public static void updateData(Connection connection) throws SQLException {
-		Scanner scanner = new Scanner(System.in);
-		PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary = ? where id =?;");
+		// Scanner scanner = new Scanner(System.in);
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("update employee_payroll set salary = ? where id =?;");
 		System.out.println("Enter salary to be updated: ");
-		double salary = scanner.nextDouble();
+		// double salary = scanner.nextDouble();
 		System.out.println("Enter at which id you want to update salary: ");
-		int id = scanner.nextInt();
-		preparedStatement.setDouble(1, salary);
-		preparedStatement.setInt(2, id);
+		// int id = scanner.nextInt();
+		preparedStatement.setDouble(1, 300000.00);
+		preparedStatement.setInt(2, 2);
 		preparedStatement.executeUpdate();
 		System.out.println("Updated Successfully.....!!!");
 	}
-	
-	 public static Double reteriveDataByName(Connection connection) throws SQLException {
-	        Double salary = null;
-	        PreparedStatement preparedStatement = connection.prepareStatement("select * from employee_payroll where name =?");
-	        preparedStatement.setString(1, "Terisa");
-	        ResultSet resultSet = preparedStatement.executeQuery();
-	        while (resultSet.next()) {
-	            salary = (resultSet.getDouble("salary"));
-	        }
-	        return salary;
-	    }
-	 
-	 public static String particularDateRange(Connection connection) throws SQLException {
-	        String name = null;
-	        String query = "select * from employee_payroll where startDate between ? and ? ";
-	        PreparedStatement preparedStatement = connection.prepareStatement(query);
-	        preparedStatement.setString(1, "2019-01-03");
-	        preparedStatement.setString(2, "2019-11-13");
-	        ResultSet resultSet = preparedStatement.executeQuery();
-	        while (resultSet.next()) {
-	            System.out.println(" ");
-	            System.out.println(resultSet.getInt(1));
-	            System.out.println(resultSet.getString(2));
-	            name = (resultSet.getString("name"));
-	        }
-	        return name;
-	    }
+
+	public static Double reteriveDataByName(Connection connection) throws SQLException {
+		Double salary = null;
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("select * from employee_payroll where name =?");
+		preparedStatement.setString(1, "Terisa");
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			salary = (resultSet.getDouble("salary"));
+		}
+		return salary;
+	}
+
+	public static String particularDateRange(Connection connection) throws SQLException {
+		String name = null;
+		String query = "select * from employee_payroll where startDate between ? and ? ";
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, "2019-01-03");
+		preparedStatement.setString(2, "2019-11-13");
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			System.out.println(" ");
+			System.out.println(resultSet.getInt(1));
+			System.out.println(resultSet.getString(2));
+			name = (resultSet.getString("name"));
+		}
+		return name;
+	}
+
+	public static Double sumByGroup(Connection connection) throws SQLException {
+		Double salary = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(
+				"select sum(salary) as salary from employee_payroll where gender = ? group by gender");
+		preparedStatement.setString(1, "F");
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			salary = (resultSet.getDouble("salary"));
+			System.out.println("Sum of salary by gender(F) is: " + salary);
+		}
+		return salary;
+	}
+
+	public static Double avgSalary(Connection connection) throws SQLException {
+		Double avgSalary = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(
+				"select avg(salary) as salary from employee_payroll where gender = ? group by gender");
+		preparedStatement.setString(1, "M");
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			avgSalary = (resultSet.getDouble("salary"));
+			System.out.println("Average salary of males is: " + avgSalary);
+		}
+		return avgSalary;
+	}
+
+	public static Double minmumSalary(Connection connection) throws SQLException {
+		Double salary = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(
+				"select min(salary) as salary from employee_payroll where gender = ? group by gender");
+		preparedStatement.setString(1, "m");
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			salary = (resultSet.getDouble("salary"));
+			System.out.println("minimum of salary by gender(m) is: " + salary);
+		}
+		return salary;
+	}
+
+	public static Double maxSalary(Connection connection) throws SQLException {
+		Double salary = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(
+				"select max(salary) as salary from employee_payroll where gender = ? group by gender");
+		preparedStatement.setString(1, "m");
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			salary = (resultSet.getDouble("salary"));
+			System.out.println("Maximum of salary by gender(m) is: " + salary);
+		}
+		return salary;
+	}
+
+	public static Integer count(Connection connection) throws SQLException {
+		Integer gender = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(
+				"select count(gender) as salary from employee_payroll where gender = ? group by gender");
+		preparedStatement.setString(1, "m");
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			gender = (resultSet.getInt("salary"));
+			System.out.println("number of gender mele is : " + gender);
+		}
+		return gender;
+	}
 
 	public static void listDrivers() {
 		Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
